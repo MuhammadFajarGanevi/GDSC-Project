ae7901a36d04c663521c834222f2afea00b383a7
oskhar <muhamad.oskhar22@mhs.uinjkt.ac.id>
2024-01-31 10:22:37 +0700

    perf: memperbaiki performa openapi

--- Documentation/index.php
+++ Documentation/index.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>OpenAPI Skema</title>
-    <link rel="stylesheet" href="swagger-ui.css" />
-    <!-- <link rel="shortcut icon" href="logo.png" type="image/x-icon" /> -->
-  </head>
-  <body>
-    <div id="swagger-ui"></div>
 
-    <script src="./swagger-ui-bundle.js"></script>
-    <script>
-      window.onload = function () {
-        const ui = SwaggerUIBundle({
-          url: "openapi.yml",
-          dom_id: "#swagger-ui",
-        });
-      };
-    </script>
-  </body>
+<head>
+  <title>OpenAPI Skema</title>
+  <link rel="stylesheet" href="swagger-ui.css" />
+  <!-- <link rel="shortcut icon" href="logo.png" type="image/x-icon" /> -->
+</head>
+
+<body>
+  <div id="swagger-ui"></div>
+
+  <script src="./swagger-ui-bundle.js"></script>
+  <script>
+    window.onload = function () {
+      const ui = SwaggerUIBundle({
+        url: "openapi-web.yml",
+        dom_id: "#swagger-ui",
+      });
+    };
+  </script>
+</body>
+
 </html>
